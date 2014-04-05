a846c2faf68c017c6377d608cec7ec0bc1509b59
Sam Eubank <sameubank+zerious@gmail.com>
2014-04-04 21:41:50 -0700

    Lighter, Chug and ltl icons.

---
+++ app.js
@@ -0,0 +1,42 @@
+var chug = require('chug');
+var app = require('express')();
+var log = console.log;
+var port = 8888;
+
+// TODO: Filter loads by file extension.
+// TODO: Wrap a JavaScript asset.
+// TODO: Allow CoffeeScript NOWRAP.
+
+app.listen(port);
+chug.setApp(app);
+log('App listening at ' + port + '.');
+
+
+chug('views')
+	.compile()
+	.then(function () {
+		console.log('Views loaded.');
+	})
+	.watch();
+
+chug('scripts')
+	.compile()
+	//.watch()
+	.concat('/all.js')
+	.route()
+	.then(function () {
+		console.log('Scripts loaded.');
+	});
+
+chug([
+	'styles/bootstrap.css',
+	'styles/font-awesome.css',
+	'styles/layout.styl'
+	])
+	.compile()
+	//.watch()
+	.concat('/all.css')
+	.route()
+	.then(function () {
+		console.log('Styles loaded.');
+	});
