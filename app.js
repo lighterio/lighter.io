fac7f9a38ab1f36627db16dc79d1cc38fa0969d3
Sam Eubank <sameubank+zerious@gmail.com>
2014-04-14 22:35:55 -0700

    Added CodeMirror textareas

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ chug.setApp(app);
 log('App listening at ' + port + '.');
 
 
+chug('public')
+	.route()
+	.watch();
+
+
 chug('views')
 	.compile()
 	.then(function () {
@@ -22,6 +27,9 @@ chug('views')
 chug([
 	'node_modules/jquery/dist/jquery.js',
 	'node_modules/codemirror/lib/codemirror.js',
+	'node_modules/codemirror/mode/javascript/javascript.js',
+	'node_modules/codemirror/mode/jade/jade.js',
+	'node_modules/codemirror/mode/htmlmixed/htmlmixed.js',
 	'node_modules/ltl/ltl.js',
 	'scripts'])
 	.compile()
@@ -39,7 +47,20 @@ chug([
 		console.log('Scripts loaded.');
 	});
 
-chug([
+ chug([
+	'node_modules/codemirror/lib/codemirror.css',
+	'node_modules/codemirror/theme/3024-night.css',
+	'node_modules/codemirror/theme/base16-dark.css',
+	'node_modules/codemirror/theme/blackboard.css',
+	'node_modules/codemirror/theme/erlang-dark.css',
+	'node_modules/codemirror/theme/lesser-dark.css',
+	'node_modules/codemirror/theme/midnight.css',
+	'node_modules/codemirror/theme/night.css',
+	'node_modules/codemirror/theme/paraiso-dark.css',
+	'node_modules/codemirror/theme/pastel-on-dark.css',
+	'node_modules/codemirror/theme/tomorrow-night-eighties.css',
+	'node_modules/codemirror/theme/twilight.css',
+	'node_modules/codemirror/theme/xq-dark.css',
 	'styles'
 	])
 	.compile()
--- scripts/ltl.coffee
+++ scripts/ltl.coffee
@@ -1,9 +1,9 @@
 $ ->
 	$document = $(document)
-	$document.on 'keyup', 'textarea._TEMPLATE,textarea._CONTEXT', ->
+	$document.on 'keydown keyup', 'textarea._TEMPLATE,textarea._CONTEXT', ->
 		$panel = $(this).closest '._PANEL'
 		template = $panel.find('._TEMPLATE').val()
-		context = $panel.find('._CONTEXT').val()
+		context = JSON.parse $panel.find('._CONTEXT').val()
 		compiled = ltl.compile template
 		output = compiled context
 		$panel.find('._COMPILED').val compiled.toString()
@@ -11,7 +11,17 @@ $ ->
 
 	$('textarea._TEMPLATE').keyup()
 
+	createEditor 'jade', $('textarea._TEMPLATE')[0]
 
-setTimeout ->
-	location.reload();
-, 60000
+
+
+createEditor = (mode, textarea) ->
+	editor = CodeMirror.fromTextArea textarea,
+		mode: mode
+		tabSize: 4
+		theme: 'blackboard'
+		lineNumbers: true
+
+	editor.on 'change', ->
+		editor.save()
+		$(textarea).keyup()
